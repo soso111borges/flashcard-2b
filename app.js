deb00faf6302f35be5f7bf2d102d7cdb7622024c
soso111borges <163007806+soso111borges@users.noreply.github.com>
2024-10-07 11:32:47 -0300

    .

---
+++ app.js
@@ -0,0 +1,15 @@
+function CriaCartao(categoria,pergunta,resposta) {
+    let container = document.getElementById('container');
+    let cartao = document.createElement('article');
+    cartao.className = 'cartao';
+}
+cartao.innerHTML = 
+<div class="cartao_conteudo">
+        <h3>Programação</h3>
+        <div class="cartao_conteudo_pergunta">
+           <p> o que é JavaScript</p>
+        </div>
+     <div class="cartao_conteudo_resposta">
+       <p>O JavaScript é uma linguagem de programação</p>
+        </div>
+        </div>
---
+++ perguntas.js
@@ -0,0 +1,41 @@
+CriaCartao(
+     "programação 2.0"
+     "O que é phyton?"
+     "phynton é uma linguagem de programação"
+)   
+
+CriaCartao(
+    "programação 2.0"
+    "quando o corinthians ganhou seu primeiro mundial?"
+    "ganhou no ano 2000"
+)   
+
+CriaCartao(
+    "programação 2.0"
+    "O que é phyton?"
+    "phynton é uma linguagem de programação"
+)   
+
+CriaCartao(
+    "programação 2.0"
+    "O que é phyton?"
+    "phynton é uma linguagem de programação"
+)   
+
+CriaCartao(
+    "programação 2.0"
+    "O que é phyton?"
+    "phynton é uma linguagem de programação"
+)   
+
+CriaCartao(
+    "programação 2.0"
+    "O que é phyton?"
+    "phynton é uma linguagem de programação"
+)   
+
+CriaCartao(
+    "programação 2.0"
+    "O que é phyton?"
+    "phynton é uma linguagem de programação"
+)   
