715c35c5f779562344b6083a567b5f20f67ca3c6
soso111borges <163007806+soso111borges@users.noreply.github.com>
2024-10-08 08:19:10 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ function CriaCartao(categoria,pergunta,resposta) {
     let container = document.getElementById('container');
     let cartao = document.createElement('article');
     cartao.className = 'cartao';
-}
-cartao.innerHTML = 
+
+    
+cartao.innerHTML = `
 <div class="cartao_conteudo">
         <h3>Programação</h3>
         <div class="cartao_conteudo_pergunta">
