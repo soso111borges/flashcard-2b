bd81b6f52497fc7cddaf22cd7edd0c7e5df0ce97
soso111borges <163007806+soso111borges@users.noreply.github.com>
2024-10-25 19:14:35 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,41 +1,41 @@
 CriaCartao(
-     "programação 2.0"
-     "quem é o maior time e que tem a melhor torcida do mundo?"
-     "CORINTHIANS"
+     'programação 2.0'
+     'quem é o maior time e que tem a melhor torcida do mundo,?',
+     'CORINTHIANS'
 )   
 
 CriaCartao(
-    "programação 2.0"
-    "Em que ano o corinthians ganhou seu primeiro paulista?"
-    "ganhou no ano de 1914"
+    'programação 2.0'
+    'Em que ano o corinthians ganhou seu primeiro paulista,?',
+    'ganhou no ano de 1914'
 )   
 
 CriaCartao(
-    "programação 2.0"
-    "o que significa CP?"
-    "significa club do povo ou corinthians paulista"
+    'programação 2.0'
+    'o que significa CP,?',
+    'significa club do povo ou corinthians paulista'
 )   
 
 CriaCartao(
-    "programação 2.0"
-    "O que aconteceu no dia 12/12/12?"
-    "corinthians ganhou seu segundo mundial"
+    'programação 2.0'
+    'O que aconteceu no dia 12/12/12,?',
+    'corinthians ganhou seu segundo mundial'
 )   
 
 CriaCartao(
-    "programação 2.0"
-    "que é o mascote do corinthians?"
-    "é um mosqueteiro"
+    'programação 2.0'
+    'Qual é o mascote do corinthians,?',
+    'é um mosqueteiro'
 )   
 
 CriaCartao(
-    "programação 2.0"
-    "quem é considerado o pior time do brasileirão?"
-    "palmeiras e são paulo"
+    'programação 2.0'
+    'Quem é considerado o pior time do brasileirão,?',
+    'palmeiras e são paulo'
 )   
 
 CriaCartao(
-    "programação 2.0"
-    "quando roger guedes volta?"
-    "NUNCA"
+    'programação 2.0'
+    'quando roger guedes volta?',
+    'NUNCA'
 )   
